6d78713a4aca01e4d25a7aacbdb8ce175c758513
Adrian Grabowski <adriangrabowski@MacBook-Pro-Adrian.local>
2023-11-01 16:02:20 +0100

    first version

---
+++ README.md
@@ -0,0 +1,265 @@
+# WhatsMyName-Client (WMNC)
+
+While attempting to develop the WhatsMyName project, I encountered several issues. Other available tools did not handle http-headers or POST requests. That's why I wrote my own script implementing the principles of WhatsMyName.
+
+My version of the client supports 'request headers' and POST requests. I tried to follow all the guidelines provided in the wmn-data-schema.json file (https://github.com/WebBreacher/WhatsMyName/blob/main/CONTRIBUTING.md#format-of-the-json-file).
+
+The script was prepared not only to search for a user with a given username but also to test newly added pages to the database.
+
+Feel free to use this script; it's open-source, so that's exactly why it was made available!
+
+<img width="333" src="https://user-images.githubusercontent.com/104733166/189120786-f854c5f8-57df-408c-bf33-b8eda521572c.png">
+
+## Installation
+
+```bash
+git clone https://github.com/grabowskiadrian/whatsmyname-client.git
+cd WhatsMyName-Client
+pip3 install -r requirments.txt
+
+python3 wmnc.py update
+python3 wmnc.py
+```
+## Usage
+
+WMNC (WhatsMyName-Client) is a command-line tool with the following commands:
+
+
+```
+$ python3 wmnc.py
+
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░▒█░░▒█░█░░░░█▀▀▄░▀█▀░█▀▀░▒█▀▄▀█░░░░░░▒█▄░▒█░█▀▀▄░█▀▄▀█░█▀▀░
+░▒█▒█▒█░█▀▀█░█▄▄█░░█░░▀▀▄░▒█▒█▒█░█▄▄█░▒█▒█▒█░█▄▄█░█░▀░█░█▀▀░
+░▒▀▄▀▄▀░▀░░▀░▀░░▀░░▀░░▀▀▀░▒█░░▒█░▄▄▄▀░▒█░░▀█░▀░░▀░▀░░▒▀░▀▀▀░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░▒█▀▀▄░█░░░▀░░█▀▀░█▀▀▄░▀█▀░░░░░    WhatsMyName-Client   ░░░░
+░▒█░░░░█░░░█▀░█▀▀░█░▒█░░█░░░░░░ v0.1 by grabowskiadrian ░░░░
+░▒█▄▄▀░▀▀░▀▀▀░▀▀▀░▀░░▀░░▀░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+Look: https://github.com/WebBreacher/WhatsMyName
+
+usage: python3 wmnc.py COMMAND [-h]
+
+Commands:
+  update           Download the newest version of wmn-data.json from the official GitHub repository.
+  find USERNAME    Search for a specific USERNAME across all websites in the Project WhatsMyName database (wmn-data.json).
+
+Development:
+  list-sites       List all websites from the wmn-data.json file.
+  list-categories  List all website categories from the wmn-data.json file.
+  test             Run tests on websites to verify that the website configuration is correct.
+```
+
+Each command supports various options, which can be displayed by using the -h or --help flag.
+
+## Examples for Searchers
+
+### Update the local database
+
+```bash
+python3 wmnc.py update
+```
+
+Output:
+```bash
+$ python3 wmnc.py update
+
+[+] wmn-data.json updated
+[+] wmn-data-schema.json updated
+```
+
+### Search for a Username
+To search for a specific username across all websites in the database, use the find command:
+
+```bash
+python3 wmnc.py find username-to-find
+```
+
+For example:
+
+```bash
+python3 wmnc.py find grabowskiadrian
+```
+
+You can use several parameters to limit the scope of the search:
+
+To search only on one website use flag **-s** or **--site**:
+```bash
+python3 wmnc.py find grabowskiadrian -s bitbucket.com
+```
+
+To search only in one category use flag **-c** or **--category**:
+```bash
+python3 wmnc.py find grabowskiadrian -c social
+```
+
+You can also use **-l** or **--list** flag to pass path to your own .json file
+
+```bash
+python3 wmnc.py find grabowskiadrian -c social -l /path/to/my-list.json
+```
+
+**Output:**
+```bash
+$ python3 wmnc.py find adriangrabowski4
+
+...
+[+] Checking cda.pl : Found : https://www.cda.pl/adriangrabowski4
+[-] Checking cfx.re : Not found
+[-] Checking championat : Not found
+[-] Checking Mastodon-Chaos.social : Not found
+[-] Checking chaturbate : Not found
+[-] Checking cHEEZburger : Not found
+[-] Checking Chamsko : Not found
+[-] Checking Chess.com : Not found
+[+] Checking Chomikuj.pl : Found : https://chomikuj.pl/adriangrabowski4/
+[+] Checking DockerHub : Found : https://hub.docker.com/v2/users/adriangrabowski4/
+...
+```
+
+
+## Examples for Contributors of WhatsMyName
+
+### List all websites
+To list all websites in the database file, use the following command:
+
+```bash
+python3 wmnc.py list-sites
+```
+Output:
+
+```bash
+$ python3 wmnc.py list-sites  
+         
+|====================================================|======================|
+| Site Name                                          | Category             |
+|====================================================|======================|
+| Mastodon-101010.pl                                 | social               |
+| 1001mem                                            | social               |
+| 3DNews                                             | social               |
+| 247sports                                          | hobby                |
+| 35photo                                            | social               |
+| 3dtoday                                            | hobby                |
+...
+...
+...
+...
+|====================================================|======================|
+| Total                                              | 630                  |
+|====================================================|======================|
+```
+
+
+If you're working on your own list of websites, you can pass -l or --list parameter to test your own .json file.
+
+```bash
+python3 wmnc.py list-sites -l /path/to/my-list.json
+```
+
+### List Website Categories
+To list website categories and the number of websites in each category, run the following command:
+
+```bash
+python3 wmnc.py list-categories
+```
+
+Output:
+
+```bash
+$ python3 wmnc.py list-categories
+|====================================================|======================|
+| Category                                           | Sites count          |
+|====================================================|======================|
+| social                                             | 195                  |
+| hobby                                              | 46                   |
+| coding                                             | 31                   |
+| xx NSFW xx                                         | 43                   |
+| finance                                            | 23                   |
+| misc                                               | 35                   |
+| blog                                               | 16                   |
+| political                                          | 13                   |
+| gaming                                             | 47                   |
+| tech                                               | 29                   |
+| art                                                | 14                   |
+| music                                              | 18                   |
+| business                                           | 23                   |
+| health                                             | 13                   |
+| shopping                                           | 23                   |
+| news                                               | 9                    |
+| video                                              | 11                   |
+| images                                             | 24                   |
+| dating                                             | 13                   |
+| archived                                           | 4                    |
+|====================================================|======================|
+| Total                                              | 630                  |
+|====================================================|======================|
+```
+
+If you're working on your own list of websites, you can pass -l or --list parameter to test your own .json file.
+
+```bash
+python3 wmnc.py list-categories -l /path/to/my-list.json
+```
+
+### Test wmn-data.json file
+
+You can test websites to verify their configurations using the **test** command. 
+
+For example, to test a specific website by name, use:
+
+```bash
+python3 wmnc.py test -s github.com
+```
+
+To test specific category, use: 
+
+```bash
+python3 wmnc.py test -c social
+```
+
+You can pass **-l** or **--list** parameter to test your own .json file 
+
+```bash
+python3 wmnc.py test -c social -l /path/to/my-list.json
+```
+
+To test all websites, use:
+
+```bash
+python3 wmnc.py test
+```
+
+A check is performed for each website. This testing process evaluates whether the responses defined for the found user and user not found cases are correct. This allows you to verify that previously defined pages still work as expected.
+
+**Output:**
+
+```bash
+$ python3 wmnc.py test
+
+| Site name                                          | User found                      | User not found                  |
+|====================================================|=================================|=================================|
+| Mastodon-101010.pl                                 | PASSED                          | PASSED                          |
+| 1001mem                                            | PASSED                          | PASSED                          |
+| 3DNews                                             | PASSED                          | PASSED                          |
+| 247sports                                          | PASSED                          | PASSED                          |
+| 35photo                                            | PASSED                          | PASSED                          |
+| 3dtoday                                            | PASSED                          | PASSED                          |
+| 7cup                                               | PASSED                          | PASSED                          |
+| 7dach                                              | PASSED                          | PASSED                          |
+| 21buttons                                          | PASSED                          | PASSED                          |
+| aaha_chat                                          | NOT PASSED                      | ???                             |
+| about.me                                           | PASSED                          | PASSED                          |
+| ACF                                                | PASSED                          | PASSED                          |
+| admire_me                                          | PASSED                          | PASSED                          |
+| Adult_Forum                                        | PASSED                          | PASSED                          |
+| adultism                                           | PASSED                          | PASSED                          |
+| ADVFN                                              | PASSED                          | PASSED                          |
+```
+
+
+## License
+
+I am a regular contributor to the Project WhatsMyName, but I am not officially part of its creation. Check WhatsMyName project for more details:
+https://github.com/WebBreacher/WhatsMyName
---
+++ wmnc.py
@@ -0,0 +1,316 @@
+import argparse
+import json
+import sys
+import requests
+
+DEFAULT_REQUEST_TIMEOUT = 10
+STANDARD_HEADERS = {
+    "Accept": "text/html, application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "accept-language": "en-US;q=0.9,en,q=0,8",
+    "accept-encoding": "gzip, deflate",
+    "user-Agent": "Mozilla/5.0 (Windows NT 10.0;Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+}
+
+COLOR_OKGREEN = '\033[92m'
+COLOR_FAIL = '\033[91m'
+COLOR_ENDC = '\033[0m'
+
+
+def action_update_json():
+    """
+    Updates the JSON data by downloading it from the official GitHub repository.
+    """
+    func_download_file("https://raw.githubusercontent.com/WebBreacher/WhatsMyName/main/wmn-data.json", "wmn-data.json")
+    func_download_file("https://raw.githubusercontent.com/WebBreacher/WhatsMyName/main/wmn-data-schema.json", "wmn-data-schema.json")
+
+
+def action_list_sites(list_path):
+    """
+    Displays the list of available websites and their categories.
+
+    :param list_path: The path to the JSON file with website data.
+    """
+    sites = func_get_sites(list_path)
+
+    print("|====================================================|======================|")
+    print("| {:<50} | {:<20} |".format('Site Name', 'Category'))
+    print("|====================================================|======================|")
+
+    for site in sites:
+        print("| {:<50} | {:<20} |".format(site['name'], site['cat']))
+
+    print("|====================================================|======================|")
+    print("| {:<50} | {:<20} |".format('Total', len(sites)))
+    print("|====================================================|======================|")
+    print("")
+
+
+def action_list_categories(list_path):
+    """
+    Displays the list of website categories and the number of websites in each category.
+
+    :param list_path: The path to the JSON file with website data.
+    """
+    sites = func_get_sites(list_path)
+    categories = {}
+
+    for site in sites:
+        if site['cat'] in categories:
+            categories[site['cat']] = categories[site['cat']] + 1
+        else:
+            categories[site['cat']] = 1
+
+    print("|====================================================|======================|")
+    print("| {:<50} | {:<20} |".format('Category', 'Sites count'))
+    print("|====================================================|======================|")
+
+    for name, count in categories.items():
+        print("| {:<50} | {:<20} |".format(name, count))
+
+    print("|====================================================|======================|")
+    print("| {:<50} | {:<20} |".format('Total', len(sites)))
+    print("|====================================================|======================|")
+    print("")
+
+
+def action_test(site_name, category_name, list_path):
+    """
+    Conducts tests on websites to verify that the website configuration is correct
+
+    :param site_name: Website name (optional).
+    :param category_name: Website category name (optional).
+    :param list_path: The path to the JSON file with website data.
+    """
+    sites = func_get_sites(list_path)
+
+    scope = []
+
+    for site in sites:
+        if site_name and site_name.lower() == site['name'].lower():
+            scope.append(site)
+
+        if category_name and category_name.lower() == site['cat'].lower():
+            scope.append(site)
+
+        if not site_name and not category_name:
+            scope.append(site)
+
+    if len(scope) == 0:
+        print("No sites to process")
+
+    print("| {:<50} | {:<31} | {:<31} |".format("Site name", "User found", "User not found"))
+    print("|====================================================|=================================|=================================|")
+    for site in scope:
+        status_correct, url, message = func_check_website(site, site['known'][0])
+        status_incorrect, url, message = func_check_website(site, "this-is-not-valid-username-mockup")
+
+        if status_correct:
+            test_correct = COLOR_OKGREEN + "PASSED" + COLOR_ENDC
+        else:
+            test_correct = COLOR_FAIL + "NOT PASSED" + COLOR_ENDC
+
+        if not status_incorrect:
+            test_incorrect = COLOR_OKGREEN + "PASSED" + COLOR_ENDC
+        else:
+            test_incorrect = COLOR_FAIL + "NOT PASSED" + COLOR_ENDC
+
+        if not status_correct:
+            test_incorrect = COLOR_FAIL + "???" + COLOR_ENDC
+
+        print("| {:<50} | {:<40} | {:<40} |".format(site['name'], test_correct, test_incorrect))
+    print("|====================================================|=================================|=================================|")
+
+
+def action_find(username, site_name, category_name, list_path):
+    """
+    Searches websites to find a specific user.
+
+    :param username: The username to search for.
+    :param site_name: Website name (optional).
+    :param category_name: Website category name (optional).
+    :param list_path: The path to the JSON file with website data.
+    """
+    sites = func_get_sites(list_path)
+
+    scope = []
+
+    for site in sites:
+        if site_name and site_name.lower() == site['name'].lower():
+            scope.append(site)
+
+        if category_name and category_name.lower() == site['cat'].lower():
+            scope.append(site)
+
+        if not site_name and not category_name:
+            scope.append(site)
+
+    if len(scope) == 0:
+        print("No sites to process")
+
+    for site in scope:
+        status, url, message = func_check_website(site, username)
+
+        if status:
+            print(f"" + COLOR_OKGREEN + "[+]" + COLOR_ENDC + " Checking " + site['name'] + " : " + COLOR_OKGREEN + message + COLOR_ENDC + " : " + url)
+        else:
+            print(f"" + COLOR_FAIL + "[-]" + COLOR_ENDC + " Checking " + site['name'] + " : " + COLOR_FAIL + message + COLOR_ENDC)
+
+
+def func_check_website(website_data, username):
+    """
+    Checks a website for the presence of a specific user.
+
+    :param website_data: Website data in dictionary format.
+    :param username: The username to check for.
+    :return: A tuple (status, url, message).
+    """
+    requestHeaders = STANDARD_HEADERS
+
+    if "headers" in website_data:
+        requestHeaders.update(website_data['headers'])
+
+    try:
+        uri_check = website_data['uri_check'].format(account=username)
+
+        if "post_body" in website_data:
+            parameters = {}
+            params = website_data['post_body'].split("&")
+            for param in params:
+                param_data = param.split("=")
+                parameters[param_data[0]] = param_data[1].format(account=username)
+
+            res = requests.post(uri_check, data=parameters, headers=requestHeaders, timeout=DEFAULT_REQUEST_TIMEOUT)
+        else:
+            res = requests.get(uri_check, headers=requestHeaders, timeout=DEFAULT_REQUEST_TIMEOUT)
+
+        if "uri_pretty" in website_data:
+            user_url = website_data['uri_pretty'].format(account=username)
+        else:
+            user_url = uri_check
+
+        estring_pos = res.text.find(website_data["e_string"]) > 0
+        estring_neg = res.text.find(website_data["m_string"]) > 0
+
+        if res.status_code == website_data['e_code'] and estring_pos:
+            return True, user_url, "Found"
+        elif res.status_code == website_data['m_code'] and estring_neg:
+            return False, "", "Not found"
+        else:
+            return None, "", "Probably not found"
+
+    except Exception as e:
+        return False, "", "Problem with request"
+
+
+def func_get_sites(list_path):
+    """
+    Loads website data from a JSON file.
+
+    :param list_path: The path to the JSON file with website data.
+    :return: A list of websites.
+    """
+    if list_path is None:
+        list_path = "wmn-data.json"
+
+    with open(list_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        sites = data["sites"]
+
+    return sites
+
+def func_download_file(url, local_filename):
+    """
+    Downloads a file from the internet and saves it locally.
+
+    :param url: The URL from which the file should be downloaded.
+    :param local_filename: The name of the file to be saved.
+    """
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        with open(local_filename, "w") as f:
+            f.write(response.text)
+        print(f"" + COLOR_OKGREEN + "[+]" + COLOR_ENDC + f" {local_filename} updated")
+    except requests.exceptions.RequestException as e:
+        print(f"" + COLOR_FAIL + "[-]" + COLOR_ENDC + f" Failed to download {local_filename}: {e}")
+
+# wrapper
+if __name__ == "__main__":
+    try:
+        print("""░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░▒█░░▒█░█░░░░█▀▀▄░▀█▀░█▀▀░▒█▀▄▀█░░░░░░▒█▄░▒█░█▀▀▄░█▀▄▀█░█▀▀░
+░▒█▒█▒█░█▀▀█░█▄▄█░░█░░▀▀▄░▒█▒█▒█░█▄▄█░▒█▒█▒█░█▄▄█░█░▀░█░█▀▀░
+░▒▀▄▀▄▀░▀░░▀░▀░░▀░░▀░░▀▀▀░▒█░░▒█░▄▄▄▀░▒█░░▀█░▀░░▀░▀░░▒▀░▀▀▀░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░▒█▀▀▄░█░░░▀░░█▀▀░█▀▀▄░▀█▀░░░░░    WhatsMyName-Client   ░░░░
+░▒█░░░░█░░░█▀░█▀▀░█░▒█░░█░░░░░░ v0.1 by grabowskiadrian ░░░░
+░▒█▄▄▀░▀▀░▀▀▀░▀▀▀░▀░░▀░░▀░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+Look: https://github.com/WebBreacher/WhatsMyName""")
+
+        if len(sys.argv) > 1 and sys.argv[1] == 'update':
+            parser = argparse.ArgumentParser(prog='python3 wmnc.py update',
+                                             description="Download the newest version of wmn-data.json from the official GitHub repository.")
+            args, unknown = parser.parse_known_args()
+
+            action_update_json()
+            exit(0)
+
+        if len(sys.argv) > 1 and sys.argv[1] == 'test':
+            parser = argparse.ArgumentParser(prog='python3 wmnc.py test',
+                                             description="Run tests on websites to verify that the website configuration is correct")
+            parser.add_argument("-s", "--site", help="")
+            parser.add_argument("-c", "--category", help="")
+            parser.add_argument("-l", "--list", help="Specify the path to a JSON file with the websites list (optional).")
+            args, unknown = parser.parse_known_args()
+
+            action_test(args.site, args.category, args.list)
+            exit(0)
+
+        if len(sys.argv) > 1 and sys.argv[1] == 'find':
+            parser = argparse.ArgumentParser(prog='python3 wmnc.py find',
+                                             description="Search all websites in the Project WhatsMyName database for a specific username.")
+            parser.add_argument("action", help="")
+            parser.add_argument("username", help="Specify the username to search for.")
+            parser.add_argument("-s", "--site", help="Specify the name of the website to search (optional).")
+            parser.add_argument("-c", "--category", help="Specify the name of the website category to search (optional).")
+            parser.add_argument("-l", "--list", help="Specify the path to a JSON file with the websites list (optional).")
+            args, unknown = parser.parse_known_args()
+
+            action_find(args.username, args.site, args.category, args.list)
+            exit(0)
+
+        if len(sys.argv) > 1 and sys.argv[1] == 'list-sites':
+            parser = argparse.ArgumentParser(prog='python3 wmnc.py list-sites',
+                                             description="List all websites from the wmn-data.json file.")
+            parser.add_argument("-l", "--list", help="Specify the path to a JSON file with the websites list (optional).")
+            args, unknown = parser.parse_known_args()
+
+            action_list_sites(args.list)
+            exit(0)
+
+        if len(sys.argv) > 1 and sys.argv[1] == 'list-categories':
+            parser = argparse.ArgumentParser(prog='python3 wmnc.py list-categories',
+                                             description="List all website categories from the wmn-data.json file.")
+            parser.add_argument("-l", "--list", help="Specify the path to a JSON file with the websites list (optional).")
+            args, unknown = parser.parse_known_args()
+
+            action_list_categories(args.list)
+            exit(0)
+
+        print("")
+
+        print("usage: python3 wmnc.py COMMAND [-h]")
+        print("\nCommands:")
+        print("  update           Download the newest version of wmn-data.json from the official GitHub repository.")
+        print("  find USERNAME    Search for a specific USERNAME across all websites in the Project WhatsMyName database (wmn-data.json).")
+        print("\nDevelopment:")
+        print("  list-sites       List all websites from the wmn-data.json file.")
+        print("  list-categories  List all website categories from the wmn-data.json file.")
+        print("  test             Run tests on websites to verify that the website configuration is correct.")
+    except KeyboardInterrupt:
+        print("")
+        print("Script stopped")
+        sys.exit(0)
